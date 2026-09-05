bcad703a303475e4e30e03c32d001aff8670cc90
Bestbrainof24 <187941507+Bestbrainof24@users.noreply.github.com>
2026-09-05 17:14:26 +0300

    refactor: use categories and portable paths

--- File_Organizer/file-organizer.py
+++ File_Organizer/file-organizer.py
@@ -4,6 +4,7 @@ import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+CURRENT_WORKING_DIR = Path.cwd()
 FILE_TYPES: dict[str, list[str]] = {
     "images": [".jpg", ".jpeg", ".png", ".gif", ".webp"],
     "videos": [".mp4", ".mkv", ".avi", ".mov"],
@@ -16,30 +17,37 @@ class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory:
             file_path = Path(event.src_path)
-            file_name = file_path.name  
+            file_name = file_path.name
             file_extension = file_path.suffix.lower()
-            # A dictionary takes file extensions & folder to move each formats to. 
+            category = ""
+
+            for category_name, extensions in FILE_TYPES.items():
+                if file_extension in extensions:
+                    category = category_name
+                    break
+            else:
+                category = "others"
+
             destination_mapping = {
-                # Key = Format/extension to handle : Value = Folder to move file format. 
-                ".zip": r"C:\Users\Precious pc\Documents\Zip files",
-                ".png": r"C:\Users\Precious pc\Documents\png_files",
-                ".psd": r"C:\Users\Precious pc\Documents\psd_destination",
-                ".pdf": r"C:\Users\Precious pc\Documents\pdf_files",
+                "images": CURRENT_WORKING_DIR / "Images",
+                "videos": CURRENT_WORKING_DIR / "Videos",
+                "audio": CURRENT_WORKING_DIR / "Audio Files",
+                "documents": CURRENT_WORKING_DIR / "Documents",
+                "archives": CURRENT_WORKING_DIR / "Archives",
+                "others": CURRENT_WORKING_DIR / "Others"
             }
-            # Default destination for unknown extensions
-            other_files = r"C:\Users\Precious pc\Documents\other_files"
-            # Get the destination directory for the file extension or use the default
-            destination = destination_mapping.get(file_extension, other_files)
+            
+            destination = destination_mapping[category]
             # Move the file to the determined destination
             # Check if file already exists in destination & delete it. 
-            if Path.exists(Path(destination).joinpath(file_name)):
+            if Path(destination).joinpath(file_name).exists():
                 print(f"File {file_name} already exists in the destination. Deleting it.")
-                Path.unlink(Path(destination).joinpath(file_name))
+                Path(destination).joinpath(file_name).unlink()
             # Move the file to the determined destination
             shutil.move(file_path, destination)
 
 if __name__ == "__main__":
-    folder_to_watch = r"C:\Users\Precious pc\Documents\monitor"  # Replace with the directory you want to monitor
+    folder_to_watch = CURRENT_WORKING_DIR / "monitor"
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path=folder_to_watch, recursive=False)  # Set recursive to True if you want to monitor subdirectories
